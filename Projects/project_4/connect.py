ac42e6e30312b825603472ffe1346e936e93581e
charan_lab <charanreddy540@gmail.com>
2026-05-06 19:46:50 +0530

    reading UAV data from telemetry

--- Projects/project_4/connect.py
+++ Projects/project_4/connect.py
@@ -1,14 +1,27 @@
 import asyncio
 from mavsdk import System
 
+
 async def main():
     drone = System()
-    await drone.connect(system_address="udp://:14540")
+    await drone.connect(system_address="udpin://0.0.0.0:14540")
     print("waiting for drone connection...")
 
     async for state in drone.core.connection_state():
         if state.is_connected:
-            print("Drone Connecte!")
+            print("Drone Connected!")
             break
+          
+    async for position in drone.telemetry.position():
+            print(f"drone_location:{position}")
+            break
+    async for flight_mode in drone.telemetry.flight_mode():
+            print(f"flight_mode:{flight_mode}")
+            break
+    async for battery in drone.telemetry.battery():
+            print(f"battery_status: {battery}")
+            break
+
+
 
 asyncio.run(main())
