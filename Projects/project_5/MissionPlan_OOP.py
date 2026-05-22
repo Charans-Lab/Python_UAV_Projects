2682b7ec877d88b2ee6163779320bbeb67298927
charan_lab <charanreddy540@gmail.com>
2026-05-22 14:04:28 +0530

    adding landing detection to Drone mission project-5

--- Projects/project_5/MissionPlan_OOP.py
+++ Projects/project_5/MissionPlan_OOP.py
@@ -11,22 +11,19 @@ class DroneMission:
         self.waypoints =[]
         self.mission_plan = None
         self.MISSION_SPEED_MS = 10.0
-        self.DGCA_ALTITUDE_LIMIT_M = 120
-       
+        self.DGCA_ALTITUDE_LIMIT_M = 120       
 
     def load_waypoints(self):
         with open(self.waypoints_location) as file:
             waypoints = [line for line in file.read().splitlines() if line.strip()]
-        print(waypoints)
-        # waypoints_list = []
+                
         for i in range(len(waypoints)):
             waypoint_number, latitude, longitude,altitude = waypoints[i].split()            
             waypoint_number = int(waypoint_number)
             latitude = float(latitude)
             longitude = float(longitude)
             altitude = float(altitude)
-            each_waypoint =[waypoint_number, latitude, longitude, altitude]
-            # waypoints_list.append(each_waypoint)
+            each_waypoint =[waypoint_number, latitude, longitude, altitude]            
             self.waypoints.append(each_waypoint)
             self.check_altitude(altitude, waypoint_number)
             
@@ -56,7 +53,7 @@ class DroneMission:
             ))
             
         self.mission_plan = MissionPlan(mission_items)
-        print(self.mission_plan)
+        
 
     async def connection_to_drone(self):
         self.drone = System()
@@ -77,7 +74,6 @@ class DroneMission:
     async def upload_mission(self):
         await self.drone.mission.clear_mission()
         await asyncio.sleep(2)
-        # print("cleared existing mission")
         await self.drone.mission.set_return_to_launch_after_mission(True)
         print("uploading mission to drone....")
         await self.drone.mission.upload_mission(self.mission_plan)
@@ -90,26 +86,30 @@ class DroneMission:
 
     async def mission_monitor(self):
         async for mission_progress in self.drone.mission.mission_progress():
-            print(mission_progress.current)
             print(f"Mission progress: {mission_progress.current}/{mission_progress.total}")
-                     
- 
+
+            if mission_progress.current == mission_progress.total:
+                print("Mission Completed, drone moving to home point")
+                async for altitude in self.drone.telemetry.altitude():                    
+                    if altitude.altitude_relative_m <= 0:
+                        print("Drone landed....")
+                        break
+                break
+
     async def run(self):
         self.load_waypoints()
         self.build_mission_plan()
         await self.connection_to_drone()
         await self.check_health()
         await self.upload_mission()
-        await asyncio.gather(self.execute_mission(), self.mission_monitor())       
+        await asyncio.gather(self.execute_mission(), self.mission_monitor())    
+        
   
 
 async def main():
-    waypoints_file = Path(__file__).parent / "waypoints.txt"
-    
-    mission = DroneMission(waypoints_file)
-    print("attempting to run script")
+    waypoints_file = Path(__file__).parent / "waypoints.txt"    
+    mission = DroneMission(waypoints_file)   
     await mission.run()
-
 
 asyncio.run(main())
 
